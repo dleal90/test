9c05947c568b0a683f53ccae481b68dba194a449
Braden Roper <bradenroper9@gmail.com>
2017-09-20 23:24:56 -0500

    Added a simple simulator, this is not necessarily tweaked to be accurate yet.

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -1,5 +1,4 @@
 import Simulator as sim
 
-# Test plot functionality
-yvalues = [2, 3, 6, 5, 8, 4, 5, 5, 5, 5, 7, 8, 9, 3, 14, 20]
-sim.DrawPlot(yvalues, "My Graph", filename="BasicPlot")
+# Simulation
+sim.SimulateBond(50, 100)
--- Scripts/Simulator.py
+++ Scripts/Simulator.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as mp
+import random
 
 
 #
 # Draws the actual plot and exports it as a png.
 #
-def DrawPlot(ys, title="", verbose=True, filename="graph"):
+def DrawPlot(ys, title="", ylab="", xlab="", verbose=True, filename="graph", useYbounds=True, ymin=0, ymax=100):
 
     if (verbose):
         print("Generating Plot...")
 
-    # Plot title
-    titleYpos = max(ys)
-    titleXpos = int((len(ys)-1)/2)
-    mp.text(titleXpos, titleYpos, title)
-
+    # Set bounds
+    if (useYbounds):
+        padding = (ymax - ymin)*.1
+        mp.ylim(ymin - padding, ymax + padding)
+        
     # Make plot
+    mp.title(title)
+    mp.xlabel(xlab)
+    mp.ylabel(ylab)
     mp.plot(ys)
 
     # Save plot
@@ -22,3 +26,31 @@ def DrawPlot(ys, title="", verbose=True, filename="graph"):
 
     if (verbose):
         print("Done!")
+
+#
+# Simulates Present Values over time and returns the
+# results as an array.
+#
+def GeneratePVs(coupon, years, noisefactor=.05):
+
+    PV = []
+    r = random.uniform(-.001, .005)
+
+    previous = coupon
+    for i in range(years):
+        noise = random.uniform(1 - noisefactor, 1 + noisefactor)
+        PV.append(previous/((1 + r)) * noise) # was (1+r**i) and "PV[i]" instead of "previous"
+        previous = PV[i]
+
+    print("r = {}".format(r))
+    print("PV = {}".format(PV))
+
+    return PV  
+
+#
+# Simulates a bond given a life-span and coupon amount.
+#
+def SimulateBond(coupon, life=10):
+    PVs = GeneratePVs(coupon, life);
+    DrawPlot(PVs, "Present Value", "US Dollars", "Years", filename="PVGraph")
+    
